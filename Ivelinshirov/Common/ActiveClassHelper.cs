8be662342d007bbc7b56d5606d48e45a2dcb0881
Hristo Shirov <hshirov@abv.bg>
2021-06-06 23:07:15 +0300

    Front-end design changes

---
+++ Ivelinshirov/Common/ActiveClassHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Linq;
+
+namespace Ivelinshirov.Common
+{
+    public static class ActiveClassHelper
+    {
+        public static string ActiveClass(this IHtmlHelper htmlHelper, string controllers = null, string actions = null, string routeId = null, string cssClass = "active")
+        {
+            var currentController = htmlHelper?.ViewContext.RouteData.Values["controller"] as string;
+            var currentAction = htmlHelper?.ViewContext.RouteData.Values["action"] as string;
+            var currentRouteId = htmlHelper?.ViewContext.RouteData.Values["id"] as string;
+
+            var acceptedControllers = (controllers ?? currentController ?? "").Split(',');
+            var acceptedActions = (actions ?? currentAction ?? "").Split(',');
+            var acceptedRouteId = (routeId ?? currentRouteId ?? "").Split(',');
+
+            if(routeId == null)
+            {
+                return acceptedControllers.Contains(currentController) && acceptedActions.Contains(currentAction)
+                ? cssClass
+                : "";
+            }
+            return acceptedControllers.Contains(currentController) && acceptedActions.Contains(currentAction)
+                && acceptedRouteId.Contains(currentRouteId)
+                ? cssClass
+                : "";
+        }
+    }
+}
--- Ivelinshirov/Models/ContactModel.cs
+++ Ivelinshirov/Models/ContactModel.cs
@@ -7,8 +7,8 @@ namespace Ivelinshirov.Models
     {
         [DisplayName("Your Name")]
         [Required(ErrorMessage = "Name is required.")]
-        [MaxLength(64, ErrorMessage = "That name is too long.")]
-        [MinLength(2, ErrorMessage = "That name is too short.")]
+        [MaxLength(64, ErrorMessage = "This name is too long.")]
+        [MinLength(2, ErrorMessage = "This name is too short.")]
         public string Name { get; set; }
         [DisplayName("Your Email")]
         [Required(ErrorMessage = "Email is required.")]
@@ -16,12 +16,12 @@ namespace Ivelinshirov.Models
         public string Email { get; set; }
         [DisplayName("Subject")]
         [Required(ErrorMessage = "Subject is required.")]
-        [MaxLength(72, ErrorMessage = "That subject is too long.")]
-        [MinLength(5, ErrorMessage = "That subject is too short.")]
+        [MaxLength(72, ErrorMessage = "This subject is too long.")]
+        [MinLength(5, ErrorMessage = "This subject is too short.")]
         public string Subject { get; set; }
         [DisplayName("Content")]
         [Required(ErrorMessage = "Content is required.")]
-        [MinLength(5, ErrorMessage = "That message is too short.")]
+        [MinLength(5, ErrorMessage = "This message is too short.")]
         public string Content { get; set; }
     }
 }
